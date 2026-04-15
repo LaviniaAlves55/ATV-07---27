270eb6e8bed2e2c199dbdcb2a734e0bfac1248f4
Lala <lavinia.alves05@escola.pr.gov.br>
2026-04-15 11:59:22 -0300

    Add files via upload

---
+++ Lavi 07-27/lavi/ex09.py
@@ -0,0 +1,10 @@
+# Verificando se um número é positivo ou negativo
+numero = int(input("Digite seu número:"))
+
+# criando uma condição
+if numero > 0:
+    print("Número positivo")
+elif numero == 0:
+    print("Número é zero")
+else:
+    print("Número negativo")
---
+++ Lavi 07-27/lavi/ex10.py
@@ -0,0 +1,5 @@
+n1 = int(input("Digite o 1° número"))
+n2 = int(input("Digite o 2° número"))
+
+if n1 > n2:
+    print()
