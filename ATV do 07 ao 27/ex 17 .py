7d529b288b94a00517b7fa5f5184652dc14d5311
Pedra <sousa.pedro27@escola.pr.gov.br>
2026-04-16 12:40:25 -0300

    Add files via upload

---
+++ ATV do 07 ao 27/ex 17 .py
@@ -0,0 +1,7 @@
+usuario = input("Usuário: ")
+senha = input("Sistema: ")
+
+if usuario == "admin" and senha == "1234":
+    print("Login realizado")
+else:
+    print("Usuário ou senha incorretos")
---
+++ ATV do 07 ao 27/ex 18 .py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número: "))
+
+if numero >= 10 and numero <= 20:
+    print("O número está entre 10 e 20")
+    
+else:
+    print("Número está entre 0 e 11 ou é maior que 21")
